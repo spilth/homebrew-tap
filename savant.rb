2e76f65cf401dd23797dbad56b567b95528f6e0d
Brian Kelly <polymonic@gmail.com>
2016-12-11 13:34:50 -0500

    Update SHA for updated file

--- savant.rb
+++ savant.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Savant < Formula
   url 'https://s3.amazonaws.com/binaries.spilth.org/savant-1.0-SNAPSHOT.tar.gz'
-  sha256 '4b33aa7f05892af043160183bbc8a40425a85ab4dffa94c5599ba0810d1e3465'
+  sha256 '29a4fba2318a678a99531bd0c438eff292d2f5245bde74668809ef23b82e3140'
 
   def install
     root = '.'
