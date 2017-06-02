d86b78d20066ce9f951ce1b6fa62709da11c4924
Brian Kelly <polymonic@gmail.com>
2017-06-02 13:01:24 -0400

    Bump Savant version for bash script fix

--- savant.rb
+++ savant.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Savant < Formula
-  url 'https://s3.amazonaws.com/binaries.spilth.org/savant/savant-06-02-17_15-19-52.tar.gz'
-  sha256 '221c1c5b51c4e3484fe56ad2a2e20ace63b895f853eb3f56d9fb89d29b4766ab'
+  url 'https://s3.amazonaws.com/binaries.spilth.org/savant/savant-06-02-17_16-59-40.tar.gz'
+  sha256 'd7d3afdd8231e59af1c5757939414cd4f8e40530dfb78b7a5e6905f5bd3a8c69'
 
   def install
     root = '.'
