0e77043429c85cf54df63a779117f4cd0e164214
Brian Kelly <polymonic@gmail.com>
2017-06-04 18:00:18 -0400

    Upgrade to 1.3.0 version of Savant

--- savant.rb
+++ savant.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Savant < Formula
-  url 'https://s3.amazonaws.com/binaries.spilth.org/savant/savant-06-02-17_16-59-40.tar.gz'
-  sha256 'd7d3afdd8231e59af1c5757939414cd4f8e40530dfb78b7a5e6905f5bd3a8c69'
+  url 'https://s3.amazonaws.com/binaries.spilth.org/savant/savant-06-04-17_21-51-43.tar.gz'
+  sha256 '3ff2596f6b2e1c0c86143dfd5f55487e9f4f1ee112ff7b4d34717388dd67e20a'
 
   def install
     root = '.'
