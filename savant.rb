6ada3206e58ac848e32c90b0144d080a6fd60707
Brian Kelly <polymonic@gmail.com>
2017-11-04 16:50:05 -0400

    Define dependencies for savant

--- savant.rb
+++ savant.rb
@@ -1,6 +1,9 @@
 require 'formula'
 
 class Savant < Formula
+  depends_on cask: 'java'
+  depends_on formula: 'maven'
+
   url 'https://s3.amazonaws.com/binaries.spilth.org/savant/savant-11-04-17_19-38-51.tar.gz'
   sha256 'fc022b134b8f7b191ef5821a88e030f357d7d00578939ef6da4d45bdb962bef8'
 
